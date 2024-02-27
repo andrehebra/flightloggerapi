724e131dbb85058cd52b440333d536ac627a49f0
andrehebra <52555960+andrehebra@users.noreply.github.com>
2024-02-26 21:56:05 -0500

    counter functional, only able to receive first 50 items, will have to investigate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,33 +5,59 @@
 
     data.latest && rates.set(data.latest);
 
-    //console.log(rates);
+    let firstName = "Jacob";
+    let lastName = "Flake";
+
+    let reservationCount = 0;
+    let cancellationCount = 0;
+    
+    let cancellationArray = [];
+    $: cancellationArray;
 
     function calculate() {
-        console.log(data);
-        let parsed = JSON.parse(data);
-        console.log(parsed);
+        reservationCount = 0;
+        cancellationCount = 0;
 
-        let dataArray = [];
-        
-        for (let i in data) {
-            dataArray.push([i, data [i]]);
-        }
+        let dataArray = [...data.data.data.bookings.nodes];
+
+        //console.log(data.data.data.bookings.nodes);
+
+        //dataArray.push(data.data.data.bookings.nodes);
+
+        //for (let i = 0; i < data.data.data.bookings.nodes.length; i++) {
+        //    dataArray.push(data.data.data.bookings.notes[i]);
+        //}
 
         console.log(dataArray);
-        console.log(dataArray[0][1]);
+
+        for (let i = 0; i < dataArray.length; i++) {
+            console.log(JSON.stringify(dataArray[i]))
+            if (JSON.stringify(dataArray[i]) == '{}') {
+
+            } else if (dataArray[i].student.firstName == firstName && dataArray[i].student.lastName == lastName){
+                reservationCount++;
+                if (dataArray[i].cancellation != null) {
+                    cancellationCount++;
+                    cancellationArray.push(dataArray[i].cancellation.title);
+                }
+            }
+        }
     }
     
 </script>
 
 
 <h1>Homepage: This page is mainly used for testing purposes</h1>
-<p><a href="https://kit.svelte.dev">Cancellation Dashboard</a></p>
+<p><a href="">Cancellation Dashboard</a></p>
 <button on:click={ calculate }>Log Data</button>
 
 <h1>Student name</h1>
 <input type="text">
 
+<p>Total Reservations: { reservationCount }</p>
+<p>Total Cancellations: { cancellationCount }</p>
 
+{#each cancellationArray as cancellationTitle}
+    <p>Cancellation Reason: { cancellationTitle }</p>
+{/each}
 
-{JSON.stringify(data)}
