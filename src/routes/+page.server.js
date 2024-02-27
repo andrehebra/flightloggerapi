6472a53a5689669828b321f28932020137f11e17
andrehebra <52555960+andrehebra@users.noreply.github.com>
2024-02-26 19:55:23 -0500

    added query, working on server, need to display it

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,8 @@ import { API_KEY } from '$env/static/private';
 
 import { error } from '@sveltejs/kit';
 
-const query = `query Query($all: Boolean) {
+const query = `
+query Query($all: Boolean) {
 	bookings(all: $all) {
 		nodes {
 			... on SingleStudentBooking {
@@ -27,3 +28,26 @@ const query = `query Query($all: Boolean) {
 }
 `;
 
+export const load = async () => {
+    try {
+        const variables = {
+            all: true,
+        };
+
+        const response = await fetch('https://api.flightlogger.net/graphql', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify({
+                query,
+                variables
+            })
+        });
+
+        const { responseData } = await response.json();
+        return { ...responseData };
+    } catch (error) {
+        console.error(`Error in load function :( ${error}`);
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script>
+    import rates from '$lib/shared/stores/rates';
+    
+    export let data;
+
+    data.latest && rates.set(data.latest);
+
+    console.log(rates[2]);
+</script>
+
+
 <h1>Homepage: This page is mainly used for testing purposes</h1>
 <p><a href="https://kit.svelte.dev">Cancellation Dashboard</a></p>
+
+<ul>hi
+{#each $rates as { startsAt, endsAt }}
+    <li>hello</li>
+{/each}
+</ul>
